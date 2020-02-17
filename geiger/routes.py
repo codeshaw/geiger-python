1e490c73055d7cc63e32fabdbc564e502a80a9fb
Tom Bradshaw <tom@codeshaw.com>
2020-02-17 02:43:45 +0000

    Working version. For now, it's complete

--- geiger/routes.py
+++ geiger/routes.py
@@ -1,19 +1,32 @@
+import json
 import logging
 from flask import jsonify, request
 from geiger import geiger
 
 logger = logging.getLogger(__name__)
 
+cpm = 0
+acpm = 0.0
+uSv = 0.0
+
 @geiger.route('/log', methods=['GET'])
 def index():
-    if 'CPM' in request.args and 'ACPM' in request.args and 'uSV' in request.args:
-        cpm = int(request.args['CPM'])
-        acpm = float(request.args['ACPM'])
-        uSV = float(request.args['uSV'])
+  if 'CPM' in request.args and 'ACPM' in request.args and 'uSV' in request.args:
+    global cpm
+    global acpm
+    global uSv
 
-        logger.info("CPM:[{0}], Average CPM:[{1:9.3f}], uSV:[{2:9.3f}]".format(cpm, acpm, uSV))
-        response = jsonify(success=True)
-        response.status_code = 200
-        return response
-    else:
-        return "Invalid input", 400
+    cpm = int(request.args['CPM'])
+    acpm = float(request.args['ACPM'])
+    uSv = float(request.args['uSV'])
+
+    logger.info("CPM:[{0}], Average CPM:[{1:9.3f}], uSV:[{2:9.3f}]".format(cpm, acpm, uSv))
+    response = "OK.ERR0"
+    return response, 200
+  else:
+    return "Invalid input", 400
+
+@geiger.route('/api/radiation-levels', methods=['GET'])
+def getRadiationLevels():
+  result = {"cpm": cpm, "acpm": acpm, "uSv": uSv}
+  return json.dumps(result)
